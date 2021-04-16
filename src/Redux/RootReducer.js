0f892b8155ec1dd3eb4d28eabf23485bce40ec7a
Ashar Ali <ashar.alia1999@gmail.com>
2021-04-16 14:15:21 +0500

    initial redux setup

---
+++ src/Redux/RootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import countryDataReducer from "./countryData/countryDataReducer/countryDataReducer";
+
+var rootReducer = combineReducers({
+    covid_Data:countryDataReducer
+  });
+  
+  export default rootReducer;
