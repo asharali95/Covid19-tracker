514bc55e4003478597cf526e9ff373bfc568fa20
Ashar Ali <ashar.alia1999@gmail.com>
2021-05-05 23:51:10 +0500

    designing error page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import "./Error.css"
+import { Grid } from "@material-ui/core";
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import "./Error.css";
 const Error = () => {
-    return (
-        <div>
-            this is error
-        </div>
-    )
-}
+  return (
+    <div className="error">
+      <Navbar />
+      <Grid className="container" container>
+        <Grid className="left" item xs={12} sm={6} />
+        <Grid className="right flex" item xs={12} sm={6}>
+          <div className="img"></div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
